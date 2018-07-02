2d9747f9bfbb39372840613be465a362a535b989
Justin Trantham <justin.trantham@netgaincloud.com>
2018-07-02 02:59:24 -0500

    Add project files.

---
+++ UserMigration/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace UserMigration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.oldUserNameTextBox = new System.Windows.Forms.TextBox();
+            this.migrateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // oldUserNameTextBox
+            // 
+            this.oldUserNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.oldUserNameTextBox.Location = new System.Drawing.Point(12, 12);
+            this.oldUserNameTextBox.Name = "oldUserNameTextBox";
+            this.oldUserNameTextBox.Size = new System.Drawing.Size(318, 35);
+            this.oldUserNameTextBox.TabIndex = 0;
+            // 
+            // migrateButton
+            // 
+            this.migrateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.migrateButton.Location = new System.Drawing.Point(12, 53);
+            this.migrateButton.Name = "migrateButton";
+            this.migrateButton.Size = new System.Drawing.Size(318, 74);
+            this.migrateButton.TabIndex = 1;
+            this.migrateButton.Text = "Migrate";
+            this.migrateButton.UseVisualStyleBackColor = true;
+            this.migrateButton.Click += new System.EventHandler(this.migrateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 139);
+            this.Controls.Add(this.migrateButton);
+            this.Controls.Add(this.oldUserNameTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "UserMigration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox oldUserNameTextBox;
+        private System.Windows.Forms.Button migrateButton;
+    }
+}
+
---
+++ UserMigration/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserMigration
+{
+    public partial class Form1 : Form
+    {
+        List<string> FoldersToCopy = new List<string>() { "Desktop", "Favorites"};
+        private string chromeLocation = @"C:\Users\";
+        private string chromeLocationAfterUser = @"\AppData\Local\Google\Chrome\User Data\Default";
+        string userName = WindowsIdentity.GetCurrent().Name;
+        private string[] CurrentUser;
+        public Form1()
+        {
+            InitializeComponent();
+             CurrentUser = userName.Split('\\');
+        }
+        private void migrateButton_Click(object sender, EventArgs e)
+        {
+            SetFullAccessPermission(@"c:\users\" + oldUserNameTextBox.Text, userName);
+
+
+            foreach (var folder in FoldersToCopy)
+            {
+                SetFullAccessPermission(@"c:\users\" + oldUserNameTextBox.Text + @"\" + folder, userName);
+                Copy(@"C:\users\" + oldUserNameTextBox.Text + @"\" + folder, @"C:\users\"+ CurrentUser[1] + @"\" + folder);
+            }
+
+            try
+            {
+                string dirSource = chromeLocation + oldUserNameTextBox.Text + chromeLocationAfterUser;
+                string dirDestination = @"C:\users\"+ CurrentUser[1] + @"\appdata\local\Google\chrome\User Data\Default";
+
+                Copy(dirSource,dirDestination);
+            }
+            catch
+            {
+                MessageBox.Show("Error Copying Chrome if Chrome is open please close");
+            }
+        }
+
+        public static void Copy(string sourceDirectory, string targetDirectory)
+        {
+            foreach (string dirPath in Directory.GetDirectories(sourceDirectory, "*",
+                SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(sourceDirectory, targetDirectory));
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(sourceDirectory, "*.*",
+                SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(sourceDirectory, targetDirectory), true);
+        }
+        public static void SetFullAccessPermission(string directoryPath, string username)
+        {
+            DirectorySecurity dir_security = Directory.GetAccessControl(directoryPath);
+
+            FileSystemAccessRule full_access_rule = new FileSystemAccessRule(username,
+                FileSystemRights.FullControl, InheritanceFlags.ContainerInherit |
+                                              InheritanceFlags.ObjectInherit, PropagationFlags.None,
+                AccessControlType.Allow);
+
+            dir_security.AddAccessRule(full_access_rule);
+
+            Directory.SetAccessControl(directoryPath, dir_security);
+        }
+    }
+}
