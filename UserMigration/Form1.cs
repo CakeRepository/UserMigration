cb7ec2b0f3c60ac9fb4ae4aaa333fa5d61a30f7b
Justin Trantham <justin.trantham@netgaincloud.com>
2018-07-02 03:06:50 -0500

    Added Admin to app

--- UserMigration/Form1.cs
+++ UserMigration/Form1.cs
@@ -1,15 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Security.AccessControl;
 using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UserMigration
