1e7f22fc9ddbc1d48aae15a9127a32e4a78b6212
Justin Trantham <takescake@outlook.com>
2018-08-06 20:54:50 -0500

    Added firefox and async process

--- UserMigration/Form1.Designer.cs
+++ UserMigration/Form1.Designer.cs
@@ -29,49 +29,106 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundPanel = new System.Windows.Forms.Panel();
+            this.copyChromeDataCheckBox = new System.Windows.Forms.CheckBox();
+            this.copyFireFoxCheckBox = new System.Windows.Forms.CheckBox();
+            this.copyUsersFolderCheckBox = new System.Windows.Forms.CheckBox();
             this.migrateButton = new System.Windows.Forms.Button();
-            this.userFoldersComboBox = new System.Windows.Forms.ComboBox();
+            this.userComboBox = new System.Windows.Forms.ComboBox();
+            this.backgroundPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // backgroundPanel
+            // 
+            this.backgroundPanel.Controls.Add(this.userComboBox);
+            this.backgroundPanel.Controls.Add(this.copyChromeDataCheckBox);
+            this.backgroundPanel.Controls.Add(this.copyFireFoxCheckBox);
+            this.backgroundPanel.Controls.Add(this.copyUsersFolderCheckBox);
+            this.backgroundPanel.Controls.Add(this.migrateButton);
+            this.backgroundPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.backgroundPanel.Location = new System.Drawing.Point(0, 0);
+            this.backgroundPanel.Name = "backgroundPanel";
+            this.backgroundPanel.Size = new System.Drawing.Size(253, 174);
+            this.backgroundPanel.TabIndex = 6;
+            // 
+            // copyChromeDataCheckBox
+            // 
+            this.copyChromeDataCheckBox.AutoSize = true;
+            this.copyChromeDataCheckBox.Location = new System.Drawing.Point(51, 62);
+            this.copyChromeDataCheckBox.Name = "copyChromeDataCheckBox";
+            this.copyChromeDataCheckBox.Size = new System.Drawing.Size(134, 17);
+            this.copyChromeDataCheckBox.TabIndex = 15;
+            this.copyChromeDataCheckBox.Text = "Copy Old Chrome Data";
+            this.copyChromeDataCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // copyFireFoxCheckBox
+            // 
+            this.copyFireFoxCheckBox.AutoSize = true;
+            this.copyFireFoxCheckBox.Location = new System.Drawing.Point(51, 85);
+            this.copyFireFoxCheckBox.Name = "copyFireFoxCheckBox";
+            this.copyFireFoxCheckBox.Size = new System.Drawing.Size(129, 17);
+            this.copyFireFoxCheckBox.TabIndex = 14;
+            this.copyFireFoxCheckBox.Text = "Copy Old Firefox Data";
+            this.copyFireFoxCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // copyUsersFolderCheckBox
+            // 
+            this.copyUsersFolderCheckBox.AutoSize = true;
+            this.copyUsersFolderCheckBox.Checked = true;
+            this.copyUsersFolderCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.copyUsersFolderCheckBox.Location = new System.Drawing.Point(51, 39);
+            this.copyUsersFolderCheckBox.Name = "copyUsersFolderCheckBox";
+            this.copyUsersFolderCheckBox.Size = new System.Drawing.Size(119, 17);
+            this.copyUsersFolderCheckBox.TabIndex = 13;
+            this.copyUsersFolderCheckBox.Text = "Copy Previous User";
+            this.copyUsersFolderCheckBox.UseVisualStyleBackColor = true;
             // 
             // migrateButton
             // 
             this.migrateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.migrateButton.Location = new System.Drawing.Point(63, 117);
+            this.migrateButton.Location = new System.Drawing.Point(32, 110);
             this.migrateButton.Margin = new System.Windows.Forms.Padding(2);
             this.migrateButton.Name = "migrateButton";
-            this.migrateButton.Size = new System.Drawing.Size(213, 48);
-            this.migrateButton.TabIndex = 1;
+            this.migrateButton.Size = new System.Drawing.Size(172, 53);
+            this.migrateButton.TabIndex = 11;
             this.migrateButton.Text = "Migrate";
             this.migrateButton.UseVisualStyleBackColor = true;
-            this.migrateButton.Click += new System.EventHandler(this.migrateButton_Click);
+            this.migrateButton.Click += new System.EventHandler(this.migrateButton_ClickAsync);
             // 
-            // userFoldersComboBox
+            // userComboBox
             // 
-            this.userFoldersComboBox.FormattingEnabled = true;
-            this.userFoldersComboBox.Location = new System.Drawing.Point(63, 54);
-            this.userFoldersComboBox.Name = "userFoldersComboBox";
-            this.userFoldersComboBox.Size = new System.Drawing.Size(213, 21);
-            this.userFoldersComboBox.TabIndex = 2;
-            this.userFoldersComboBox.SelectedIndexChanged += new System.EventHandler(this.userFoldersComboBox_SelectedIndexChanged);
+            this.userComboBox.FormattingEnabled = true;
+            this.userComboBox.Location = new System.Drawing.Point(13, 13);
+            this.userComboBox.Name = "userComboBox";
+            this.userComboBox.Size = new System.Drawing.Size(228, 21);
+            this.userComboBox.TabIndex = 16;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(363, 211);
-            this.Controls.Add(this.userFoldersComboBox);
-            this.Controls.Add(this.migrateButton);
+            this.ClientSize = new System.Drawing.Size(253, 174);
+            this.Controls.Add(this.backgroundPanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(269, 213);
+            this.MinimumSize = new System.Drawing.Size(269, 213);
             this.Name = "Form1";
             this.Text = "UserMigration";
+            this.backgroundPanel.ResumeLayout(false);
+            this.backgroundPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel backgroundPanel;
+        private System.Windows.Forms.CheckBox copyChromeDataCheckBox;
+        private System.Windows.Forms.CheckBox copyFireFoxCheckBox;
+        private System.Windows.Forms.CheckBox copyUsersFolderCheckBox;
         private System.Windows.Forms.Button migrateButton;
-        private System.Windows.Forms.ComboBox userFoldersComboBox;
+        private System.Windows.Forms.ComboBox userComboBox;
     }
 }
 
--- UserMigration/Form1.cs
+++ UserMigration/Form1.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Security.AccessControl;
 using System.Security.Principal;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace UserMigration
@@ -14,6 +16,7 @@ namespace UserMigration
         private string chromeLocationAfterUser = @"\AppData\Local\Google\Chrome\User Data\Default";
         string userName = WindowsIdentity.GetCurrent().Name;
         private string[] CurrentUser;
+        private string oldUser;
         public Form1()
         {
             InitializeComponent();
@@ -24,40 +27,50 @@ namespace UserMigration
             CurrentUser = userName.Split('\\');
             folderInit();
         }
-        private void migrateButton_Click(object sender, EventArgs e)
+        
+        async Task<int> doWork()
         {
-            SetFullAccessPermission(@"c:\users\" + userFoldersComboBox.Text, userName);
 
-
-            foreach (var folder in FoldersToCopy)
+            SetFullAccessPermission(@"c:\users\" + oldUser, userName);
+            if (copyUsersFolderCheckBox.Checked)
             {
-                SetFullAccessPermission(@"c:\users\" + userFoldersComboBox.Text + @"\" + folder, userName);
-                Copy(@"C:\users\" + userFoldersComboBox.Text + @"\" + folder, @"C:\users\"+ CurrentUser[1] + @"\" + folder);
+                foreach (var folder in FoldersToCopy)
+                {
+                    SetFullAccessPermission(@"c:\users\" + oldUser + @"\" + folder, userName);
+                    Copy(@"C:\users\" + oldUser + @"\" + folder, @"C:\users\" + CurrentUser[1] + @"\" + folder);
+                }
             }
-
-            try
+            if (copyChromeDataCheckBox.Checked)
             {
-                string dirSource = chromeLocation + userFoldersComboBox.Text + chromeLocationAfterUser;
-                string dirDestination = @"C:\users\"+ CurrentUser[1] + @"\appdata\local\Google\chrome\User Data\Default";
+                try
+                {
+                    string dirSource = chromeLocation + oldUser + chromeLocationAfterUser;
+                    string dirDestination = @"C:\users\" + CurrentUser[1] + @"\appdata\local\Google\chrome\User Data\Default";
 
-                Copy(dirSource,dirDestination);
+                    Copy(dirSource, dirDestination);
+                }
+                catch
+                {
+                    MessageBox.Show("Error Copying Chrome if Chrome is open please close");
+                }
             }
-            catch
+            if (copyFireFoxCheckBox.Checked)
             {
-                MessageBox.Show("Error Copying Chrome if Chrome is open please close");
+                transferFirefox();
             }
+            return 0;
         }
-
         public static void Copy(string sourceDirectory, string targetDirectory)
         {
             foreach (string dirPath in Directory.GetDirectories(sourceDirectory, "*",
                 SearchOption.AllDirectories))
                 Directory.CreateDirectory(dirPath.Replace(sourceDirectory, targetDirectory));
-
             //Copy all the files & Replaces any files with the same name
             foreach (string newPath in Directory.GetFiles(sourceDirectory, "*.*",
                 SearchOption.AllDirectories))
+            {
                 File.Copy(newPath, newPath.Replace(sourceDirectory, targetDirectory), true);
+            }
         }
         public static void SetFullAccessPermission(string directoryPath, string username)
         {
@@ -79,7 +92,7 @@ namespace UserMigration
             var folders = dirSplitByFolder(dirsIndirectory);
             foreach (var folder in folders)
             {
-                userFoldersComboBox.Items.Add(folder);
+                userComboBox.Items.Add(folder);
             }
         }
         
@@ -94,9 +107,34 @@ namespace UserMigration
             return dir; 
         }
 
-        private void userFoldersComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        private void transferFirefox()
         {
+            var newFirefoxLocation =  @"C:\Users\"+ CurrentUser[1] + @"\AppData\Roaming\Mozilla\Firefox";
+            var oldFirefoxLocation = @"C:\Users\" + oldUser + @"\AppData\Roaming\Mozilla\Firefox";
+            //Copies ini
+            Copy(oldFirefoxLocation,newFirefoxLocation);
+        }
 
+        private async void migrateButton_ClickAsync(object sender, EventArgs e)
+        {
+            oldUser = userComboBox.Text;
+            Panel p = new Panel();
+            p.Dock = DockStyle.Fill;
+            p.BackColor = Color.White;
+            p.Visible = true;
+
+            Label loading = new Label();
+            loading.Text = "Loading";
+            loading.Dock = DockStyle.Top;
+            p.Controls.Add(loading);
+            
+            backgroundPanel.Controls.Add(p);
+            p.BringToFront();
+            migrateButton.Enabled = false;
+            await Task.Run(() => doWork());
+            migrateButton.Enabled = true;
+
+            p.Dispose();
         }
     }
 }
